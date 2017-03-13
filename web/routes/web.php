e53e809bc34d9494596e288a132865f5d60e941f
Vusal317 <vusaltrue@gmail.com>
2017-03-13 18:13:43 +0400

    Update web.php

--- web/routes/web.php
+++ web/routes/web.php
@@ -10,16 +10,15 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('/', function(){return"";});
 
 Route::post('/login', 'ApiController@postLogin');
-
 Route::get('/user-info/{id}', 'ApiController@getUserInfo');
-
 Route::get('/cust-list/{id}', 'ApiController@custList');
-
 Route::get('/uni-ant-codes-by-cat', 'ApiController@codesByCat');
-
-Route::get('/add', 'ApiController@postAdd');
+Route::post('/add', 'ApiController@postAdd');
+Route::get('/custumer/{cust_code}', 'ApiController@custumer');
+Route::get('/sales/{id}', 'ApiController@sales');
 
 
 
